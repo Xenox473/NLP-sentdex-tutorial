d9115878efe71a5ac80342ea7bbe7485fb0e2814
Xenox473 <abhishek473@gmail.com>
2019-06-05 12:46:28 -0400

    Initial commit

---
+++ codeAlong.py
@@ -0,0 +1,31 @@
+# PART 1 : Sentence and word tokenizing
+
+# import nltk
+
+# nltk.download()
+
+# tokenizing - word tokenizers and sentence tokenizers
+# lexicon and corporas
+# corpora - body of text
+# lexicon - words and their means
+
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+example_text = "Hello there, how are you doing today? The weather is great. Indeed it is. I am doing well."
+
+print(sent_tokenize(example_text))
+print(word_tokenize(example_text))
+
+#  PART 2 : Stop Words
+
+from nltk.corpus import stopwords
+
+example_sentence = "This is an example showing off stop word filtration"
+stop_words = set(stopwords.words("english"))
+
+words = word_tokenize(example_sentence)
+filtered_sentence = []
+for w in words:
+    if w not in stop_words:
+        filtered_sentence.append(w)
+print (filtered_sentence)
